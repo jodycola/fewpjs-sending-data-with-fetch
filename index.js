c94c32231810dae5b74c05a68819cf773ae9713b
Kody Samaroo <github email address>
2021-02-07 01:49:49 -0500

    commit tree 72161d313f079c5b555179744a26d6be7c3ba028

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+    const url = `http://localhost:3000/users`
+    let newUser = {name, email}
+
+    return fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(newUser)
+    })
+    .then(res => res.json())
+    .then(function(user) {
+        let p = document.createElement("p")
+        p.textContent = user.id
+        document.body.append(p)
+    })
+    .catch(function(error) {
+        let p = document.createElement("p")
+        p.textContent = error.message
+        document.body.append(p)
+    })
+}   
